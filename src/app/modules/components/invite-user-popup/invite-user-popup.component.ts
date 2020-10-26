8fcbbce427679805a57db956f5bf9055b546a029
nika2212 <nikabarsukovi@gmail.com>
2020-10-27 02:39:16 +0400

    first commit

---
+++ src/app/modules/components/invite-user-popup/invite-user-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invite-user-popup',
+  templateUrl: './invite-user-popup.component.html',
+  styleUrls: ['./invite-user-popup.component.scss']
+})
+export class InviteUserPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/components/remove-user-confirm-popup/remove-user-confirm-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-remove-user-confirm-popup',
+  templateUrl: './remove-user-confirm-popup.component.html',
+  styleUrls: ['./remove-user-confirm-popup.component.scss']
+})
+export class RemoveUserConfirmPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
